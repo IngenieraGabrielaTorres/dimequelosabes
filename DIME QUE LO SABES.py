23ef82ba1bb2d4154ee0326e7067516e25095300
gabrielatorresramos12 <105952222+gabrielatorresramos12@users.noreply.github.com>
2022-05-28 15:18:38 -0500

    Add files via upload

---
+++ DIME QUE LO SABES.py
@@ -0,0 +1,37 @@
+#Taller 1
+#Dime que lo sabes 
+#Gariela Torres
+#ID:1001970935
+#ID:502193
+#correo:gabriela.torresr@correo.upb.edu.co
+#Cel:3234708201
+#Diplomado de PYTHON APLICADO A LA INGENIERIA 
+ 
+
+#Defino mi variables
+
+a= 100
+b= 200
+c= 300
+d= 400
+e= 500
+f= 600
+
+#calculo la expresion matematica
+
+ecu1= ((a+(b/c))/(d+(e/f)))
+
+#calculo la expresion matematica
+    
+Ecu2= ( a - ( b / ( c - d )))
+
+#cambiodevariables
+ecu1,Ecu2 = Ecu2,ecu1
+
+#imprimirvalores de la ecuacion 1 y la ecuacion 2
+
+print ( ecu1 )
+print ( Ecu2 )
+
+
+
